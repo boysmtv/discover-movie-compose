b4e3900808034cf5761610b74cdcecfe70a67a28
Dedy Wijaya <Boys.mtv@gmail.com>
2026-02-11 14:30:52 +0700

    chore: update gradle

--- nav/build.gradle.kts
+++ nav/build.gradle.kts
@@ -62,7 +62,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.ui.tooling.preview)
-//    implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
 
     implementation(libs.androidx.material3)
